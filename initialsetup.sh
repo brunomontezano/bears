082a1ce94e6952c65e4ba89ab448bb9d985f7a01
Bruno Montezano <brunodoyt@gmail.com>
2020-07-14 22:48:35 -0300

    Add package to be installed.

--- initialsetup.sh
+++ initialsetup.sh
@@ -19,17 +19,18 @@ passwd $USER
 echo "%wheel ALL=(ALL) NO PASSWD: ALL" >> /etc/sudoers
 
 
-pacman --noconfirm --needed -S base-devel xorg-server xorg-xinit i3-gaps i3status i3blocks i3lock feh ffmpeg arandr pavucontrol pamixer mpv wget python-pip vim htop newsboat networkmanager imagemagick transmission-gtk zathura zathura-pdf-mupdf youtube-dl unzip unrar scrot git neovim lxappearance exa cmus alsa-utils arandr elinks qutebrowser picom pulseaudio redshift sxiv zip vifm udisks2 usbutils ttf-hack ttf-liberation ttf-dejavu man-db man-pages galculator exfat-utils dmenu dialog nmap ttf-font-awesome ttf-joypixels deadbeef arch-wiki-docs arch-wiki-lite nano pulseaudio-alsa alacritty audacity rsync
+pacman --noconfirm --needed -S base-devel xorg-server xorg-xinit i3-gaps i3status i3blocks i3lock feh ffmpeg arandr pavucontrol pamixer mpv wget python-pip vim htop newsboat networkmanager imagemagick transmission-gtk zathura zathura-pdf-mupdf youtube-dl unzip unrar scrot git neovim lxappearance exa cmus alsa-utils arandr elinks qutebrowser picom pulseaudio redshift sxiv zip vifm udisks2 usbutils ttf-hack ttf-liberation ttf-dejavu man-db man-pages galculator exfat-utils dmenu dialog nmap ttf-font-awesome ttf-joypixels deadbeef arch-wiki-docs arch-wiki-lite nano pulseaudio-alsa alacritty audacity rsync rofi
 
 systemctl enable NetworkManager
 systemctl start NetworkManager
 
 pip install i3ipc 
 
-git clone https://github.com/brunomontezano/brunoarch > /home/$USER/
+git clone https://github.com/brunomontezano/bears > /home/$USER/
+chown -R $USER:$USER /home/$USER/bears
 
 echo "Root system configuration is complete!"
 echo ""
 echo "Now you can log out of root user (Ctrl + D), log in as $USER and run the following command:"
 echo ""
-echo "bash /home/$USER/brunoarch/finalsetup.sh"
+echo "bash /home/$USER/bears/finalsetup.sh"
