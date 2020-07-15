0631a150cfc4c8c9227ee99c547d17417d700a9b
Bruno Montezano <brunodoyt@gmail.com>
2020-07-15 01:34:19 -0300

    Add coments to the scripts.

--- finalsetup.sh
+++ finalsetup.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+# Start pulseaudio
 pulseaudio --start
 
+# Yay AUR Helper Installation Process
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg --noconfirm -si
 cd ..
 rm -rf yay/
 
+# Installing some fonts and other programs via yay
 yay --confirm -S nerd-fonts-mononoki ttf-bitstream-vera shell-color-scripts pspp zotero
 
+# Clone my dotfiles (configuration files) repository
 git clone https://github.com/brunomontezano/dotfiles
+
+# Make the master&stack layout script executable
 chmod +x ~/dotfiles/.config/i3/scripts/autolayout.py
+
+# Transfer my dotfiles to the user home directory
 rsync -va --delete-after dotfiles/ ~
 
+# Install Vundle (Vim Plugin Manager)
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# Create .xinitrc and put i3 to run when Xorg session starts
 echo "exec i3" >> ~/.xinitrc
 
 echo "And we are done! The installation is complete!"
--- initialsetup.sh
+++ initialsetup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Here I setup a little loop with while to read the username that the user is inserting and confirming it
 pause=1
 
 while [ $pause == 1 ]
@@ -14,19 +15,29 @@ do
     esac
 done
 
+# Then, I create the user, create a home directory, add it to the wheel group and set bash as the default shell
 useradd -m -g wheel -s /bin/bash $USER
+
+# Create a password for the new user
 passwd $USER
+
+# Add the new user to sudoers file via the wheel group
 echo "%wheel ALL=(ALL) NO PASSWD: ALL" >> /etc/sudoers
 
-
+# Install the packages needed for my setup
 pacman --noconfirm --needed -S base-devel xorg-server xorg-xinit i3-gaps i3status i3blocks i3lock feh ffmpeg arandr pavucontrol pamixer mpv wget python-pip vim htop newsboat networkmanager imagemagick transmission-gtk zathura zathura-pdf-mupdf youtube-dl unzip unrar scrot git neovim lxappearance exa cmus alsa-utils arandr elinks qutebrowser picom pulseaudio redshift sxiv zip vifm udisks2 usbutils ttf-hack ttf-liberation ttf-dejavu man-db man-pages galculator exfat-utils dmenu dialog nmap ttf-font-awesome ttf-joypixels deadbeef arch-wiki-docs arch-wiki-lite nano pulseaudio-alsa alacritty audacity rsync rofi
 
+# Enable and start the Network Manager service
 systemctl enable NetworkManager
 systemctl start NetworkManager
 
+# Install i3ipc via pip to make my master&stack script work on i3
 pip install i3ipc 
 
+# Clone the BEARS repository to the new user home directory
 git clone https://github.com/brunomontezano/bears > /home/$USER/
+
+# Change the ownership of the repository to the new user
 chown -R $USER:$USER /home/$USER/bears
 
 echo "Root system configuration is complete!"
